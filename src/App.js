d874d489607011cc489a124e949dd2853be07f41
Aman Singh Bhogal <bhogalamansingh22@gmail.com>
2021-12-01 12:05:09 +0530

    Home page Modified

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import Home from './Components/Home';
 import NavBar from './Components/NavBar';
+// import BlankCard from './Components/BlankCard';
 
 function App() {
   return (
