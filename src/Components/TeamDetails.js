e17189bc5fe6da648cd81cbe8f268304ae54ef37
Aman Singh Bhogal <bhogalamansingh22@gmail.com>
2021-12-08 00:01:59 +0530

    Team component bug fix 1.0

--- src/Components/TeamDetails.js
+++ src/Components/TeamDetails.js
@@ -1,7 +1,7 @@
 const TeamMembers = [
     {
         "name": 'Shambhavi Gupta', 
-        "image": '/images/team/Shambhavi Gupta.jpg', 
+        "image": '/images/Team/Shambhavi Gupta.jpg', 
         "role": 'GDSC Lead', 
         "insta": '', 
         "linkedin": 'https://www.linkedin.com/in/shambhavigupta2411/' 
@@ -44,28 +44,28 @@ const TeamMembers = [
     // Event Management Team.
     {
         "name": 'Divya Bhanushali',
-        "image": '/images/team/Divya Kheraj Bhanushali.jpg',
+        "image": '/images/Team/Divya Kheraj Bhanushali.jpg',
         "role": 'Event Management Lead',
         "insta": 'https://www.instagram.com/divy_b/',
         "linkedin": 'https://www.linkedin.com/in/divya-bhanushali-3a6725113/'
     },
     {
         "name": 'Akshat Kotian',
-        "image": '/images/team/Akshat Kotian.jpeg', 
+        "image": '/images/Team/Akshat Kotian.jpeg', 
         "role": '', 
         "insta": 'https://www.instagram.com/_akshatkotian04/', 
         "linkedin": 'https://www.linkedin.com/in/akshatkotian/'
     },
     {
         "name": 'Aryan Sharma',
-        "image": '/images/team/Aryan Sharma.jpg',
+        "image": '/images/Team/Aryan Sharma.jpg',
         "role": '',
         "insta": 'https://www.instagram.com/aryansharma_202/',
         "linkedin": 'https://www.linkedin.com/in/aryan-sharma-219a39193/'
     },
     {
         "name": 'Mithali Patil',
-        "image": '/images/team/Mithali Patil.png',
+        "image": '/images/Team/Mithali Patil.png',
         "role": '',
         "insta": 'https://www.instagram.com/mithalipatill/',
         "linkedin": ''
@@ -87,42 +87,42 @@ const TeamMembers = [
     // Now Marketing Team
     {
         "name": 'ANANYA SINGH NIRWAN',
-        "image": '/images/team/ANANYA SINGH NIRWAN.jpg',
+        "image": '/images/Team/ANANYA SINGH NIRWAN.jpg',
         "role": 'Marketing Lead',
         "insta": 'https://www.instagram.com/ananyanirwan/',
         "linkedin": 'https://www.linkedin.com/in/ananya-singh-nirwan-977a1819a/'
     },
     {
         "name": 'Hridayank Sharma',
-        "image": '/images/team/Hridayank Sharma .jpg',
+        "image": '/images/Team/Hridayank Sharma .jpg',
         "role": '',
         "insta": 'https://www.instagram.com/hridayank_sharma/',
         "linkedin": ''
     },
     {
         "name": 'Krrish Juneja',
-        "image": '/images/team/Krrish Juneja.jpg',
+        "image": '/images/Team/Krrish Juneja.jpg',
         "role": '',
         "insta": 'https://www.instagram.com/junejakrrish/',
         "linkedin": 'https://www.linkedin.com/in/krrish-juneja-470b981a8/'
     },
     {
         "name": 'Muskan Chopra',
-        "image": '/images/team/Muskan Chopra.jpeg',
+        "image": '/images/Team/Muskan Chopra.jpeg',
         "role": '',
         "insta": 'https://www.instagram.com/muskanchopraaa/',
         "linkedin": 'https://www.linkedin.com/in/muskan-chopra-787b29199/'
     },
     {
         "name": 'Pradhumn Bhardwaj',
-        "image": '/images/team/Pradhumn Bhardwaj.jpg',
+        "image": '/images/Team/Pradhumn Bhardwaj.jpg',
         "role": '',
         "insta": 'https://www.instagram.com/pradhumn_4/',
         "linkedin": 'https://www.linkedin.com/in/pradhumn-bhardwaj-6b9a5221b/'
     },
     {
         "name": 'Mitansh Wadhwani',
-        "image": '/images/team/MitanshWadhwani.jpg',
+        "image": '/images/Team/MitanshWadhwani.jpg',
         "role": '',
         "insta": 'https://www.instagram.com/mwadhwani_04/',
         "linkedin": 'https://www.linkedin.com/in/mitansh-wadhwani-0752511b7/'
@@ -130,42 +130,42 @@ const TeamMembers = [
     // Technical Team
     {
         "name": 'Aman Singh Bhogal',
-        "image": '/images/team/Aman Singh Bhogal.jpg',
+        "image": '/images/Team/Aman Singh Bhogal.jpg',
         "role": 'Technical Lead',
         "insta": 'https://www.instagram.com/asb_0002/',
         "linkedin": 'https://www.linkedin.com/in/aman-singh-bhogal-002914195/'
     },
     {
         "name": 'Aditya Ravi',
-        "image": '/images/team/Aditya Ravi.jpg',
+        "image": '/images/Team/Aditya Ravi.jpg',
         "role": '',
         "insta": 'https://www.instagram.com/adman1912/',
         "linkedin": 'https://www.linkedin.com/in/adman1917/'
     },
     {
         "name": 'Vasu Choudhary',
-        "image": '/images/team/Vasu Choudhary.jpeg',
+        "image": '/images/Team/Vasu Choudhary.jpeg',
         "role": '',
         "insta": 'https://www.instagram.com/vasu0508/',
         "linkedin": 'https://www.linkedin.com/in/vasu-choudhary-59a3b0176/'
     },
     {
         "name": 'Yash Garg',
-        "image": '/images/team/Yash Garg.jpeg',
+        "image": '/images/Team/Yash Garg.jpeg',
         "role": '',
         "insta": 'https://www.instagram.com/yolo__yash/',
         "linkedin": 'https://www.linkedin.com/in/yashg-23/'
     },
     {
         "name": 'Nupoor khatri',
-        "image": '/images/team/Nupoor khatri.jpg',
+        "image": '/images/Team/Nupoor khatri.jpg',
         "role": '',
         "insta": 'https://www.instagram.com/nupoor_khatri/',
         "linkedin": 'https://www.linkedin.com/in/nupoor-khatri-1bb61a1a3/'
     },
     {
         "name": 'Priyanshu Mahajan',
-        "image": '/images/team/Priyanshu Mahajan.jpg',
+        "image": '/images/Team/Priyanshu Mahajan.jpg',
         "role": '',
         "insta": 'https://www.instagram.com/_priyanshu.30_/',
         "linkedin": 'https://www.linkedin.com/in/priyanshu-mahajan/'
@@ -173,42 +173,42 @@ const TeamMembers = [
     // Creative Team
     {
         "name": 'Manan Takkar',
-        "image": '/images/team/Manan Takkar.jpeg',
+        "image": '/images/Team/Manan Takkar.jpeg',
         "role": 'Creative Lead',
         "insta": 'https://www.instagram.com/manantakkar18/',
         "linkedin": ''
     },
     {
         "name": 'Jainil Modi',
-        "image": '/images/team/Jainil Modi.jpg',
+        "image": '/images/Team/Jainil Modi.jpg',
         "role": '',
         "insta": 'https://www.instagram.com/_modi.j/',
         "linkedin": ''
     },
     {
         "name": 'Sayli Karwade',
-        "image": '/images/team/Sayli Karwade.jpg',
+        "image": '/images/Team/Sayli Karwade.jpg',
         "role": '',
         "insta": 'https://www.instagram.com/saylikarwade/',
         "linkedin": 'https://www.linkedin.com/in/sayli-karwade-6860b7225/'
     },
     {
         "name": 'Rushi Kamble',
-        "image": '/images/team/Rushi Kamble.jpeg',
+        "image": '/images/Team/Rushi Kamble.jpeg',
         "role": '',
         "insta": 'https://www.instagram.com/imrushikamble/',
         "linkedin": 'https://www.linkedin.com/in/rushi-kamble-06b86020b/'
     },
     {
         "name": 'Saniddhya Dubey',
-        "image": '/images/team/Saniddhya Dubey.jpg',
+        "image": '/images/Team/Saniddhya Dubey.jpg',
         "role": '',
         "insta": 'https://www.instagram.com/10_saniddhya/',
         "linkedin": 'https://www.linkedin.com/in/saniddhya-dubey-69a9a520b/'
     },
     {
         "name": 'Tanush Banchhod',
-        "image": '/images/team/Tanush.jpg',
+        "image": '/images/Team/Tanush.jpg',
         "role": '',
         "insta": 'https://www.instagram.com/___.tanu.____/',
         "linkedin": 'https://www.linkedin.com/in/tanush-banchhod-633074201/'
@@ -216,28 +216,28 @@ const TeamMembers = [
     // Public Relation Team
     {
         "name": 'Samarth Varma',
-        "image": '/images/team/samarth_varma.jpg',
+        "image": '/images/Team/samarth_varma.jpg',
         "role": 'Public Relations Lead',
         "insta": 'https://www.instagram.com/samarth_varma/',
         "linkedin": 'https://www.linkedin.com/in/samarth-varma-946223144/'
     },
     {
         "name": 'MANISHKA PARETA',
-        "image": '/images/team/MANISHKA PARETA.jpg',
+        "image": '/images/Team/MANISHKA PARETA.jpg',
         "role": '',
         "insta": 'https://www.instagram.com/manishkapareta/',
         "linkedin": 'https://www.linkedin.com/in/manishka-pareta-4816121ba/'
     },
     {
         "name": 'Sam Varghese',
-        "image": '/images/team/Sam Varghese.jpeg',
+        "image": '/images/Team/Sam Varghese.jpeg',
         "role": 'NMIMS MPSTME, Indore',
         "insta": 'https://www.instagram.com/__sam_varghese__/',
         "linkedin": 'https://www.linkedin.com/in/sam-varghese-1a7701209/'
     },
     {
         "name": 'Shruti Rai',
-        "image": '/images/team/Shruti Rai.jpeg',
+        "image": '/images/Team/Shruti Rai.jpeg',
         "role": '',
         "insta": '',
         "linkedin": 'https://www.linkedin.com/in/shruti-rai-624421306/'
